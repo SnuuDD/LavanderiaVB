598505b79d26e0fdf52b7bb895a7e5bdd8e3f36e
SnuuDD <mj.jesusj@gmail.com>
2020-09-03 14:21:35 -0500

    LavanderiaVB

---
+++ Lavanderia/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub ClienteBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClienteBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClienteBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.LavanderiaDataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'LavanderiaDataSet.Cliente' Puede moverla o quitarla según sea necesario.
+        Me.ClienteTableAdapter.Fill(Me.LavanderiaDataSet.Cliente)
+
+    End Sub
+End Class
